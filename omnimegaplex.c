c155a459be8554f50aa9f4318b3583a1e712f89c
JakeLinux <jake.leavitt74@gmail.com>
2023-02-07 20:41:09 -0500

    everything but soccer players

--- omnimegaplex.c
+++ omnimegaplex.c
@@ -19,10 +19,20 @@ sem_t soccer;
 sem_t fieldAvail;
 sem_t game;
 
-int pNum = 0;
+int playersLeft;
+int numBball = 0;
+int numSoccer = 0;
+int numFootball = 0;
 int avail = 1;
-int baseballLength;
-int footballLength;
+int bballPlaytime;
+int footballPlaytime;
+
+int donePlaying = 0;
+int onField = 0;
+
+
+
+struct timespec start;
 
 struct player{
     int pType;
@@ -39,8 +49,42 @@ void playerGo(void* args){
 
     switch(p->pType){
         case BASEBALL:
+            printf("[Baseball %d] I want to play baseball!\n", p->id);
+            numBball++;
+            sem_wait(&bball);
+            int pos = 19-playersLeft;
+            printf("[Baseball %d] I'm going on the field in position [%d]", p->id, pos);
+            playersLeft--;
+            if(playersLeft > 0)
+                sem_post(&bball);
+            sleep(bballPlaytime);
+            sem_wait(&game);
+            donePlaying++;
+            printf("[Baseball %d] I finished playing!");
+            if(donePlaying == 18){
+                avail = 1;
+                sem_post(&fieldAvail);
+                donePlaying = 0;
+            }
             break;
         case FOOTBALL:
+            printf("[Football %d] I want to play ootball!\n", p->id);
+            numFootball++;
+            sem_wait(&football);
+            int pos = 23-playersLeft;
+            printf("[Football %d] I'm going on the field in position [%d]", p->id, pos);
+            playersLeft--;
+            if(playersLeft > 0)
+                sem_post(&football);
+            sleep(footballPlaytime);
+            sem_wait(&game);
+            donePlaying++;
+            printf("[Football %d] I finished playing!");
+            if(donePlaying == 22){
+                avail = 1;
+                sem_post(&fieldAvail);
+                donePlaying = 0;
+            } 
             break;
         case SOCCER:
             break;
@@ -49,6 +93,37 @@ void playerGo(void* args){
 
 
 
+}
+
+int getTheField(){
+    sem_wait(&fieldAvail);
+    int gameType = getRand(1,3);
+    if(numBball > 17 && gameType == BASEBALL){
+        bballPlaytime-=18;
+        playersLeft = 18;
+        avail = 0;
+        sem_post(&bball);
+        return 1;
+    }
+    else if(footballPlaytime > 21 && gameType == FOOTBALL){
+        footballPlaytime-=22;
+        playersLeft = 22;
+        avail = 0;
+        sem_post(&football);
+        return 1;
+    }
+    else if(numSoccer%2 == 0 && gameType == SOCCER){
+        numSoccer-=2;
+        avail = 0;
+        onField=  2;
+        clock_gettime(CLOCK_REALTIME, &start);
+        sem_post(&soccer);
+        sem_post(&soccer);
+        return 1;
+    }
+    //Not enough players
+    sem_post(&fieldAvail);
+    return 0;
 }
 
 int main(){
@@ -61,7 +136,8 @@ int main(){
     sem_init(&fieldAvail, 0, 0);
     sem_init(&game, 0, 0);
 
-    baseballLength = getRand(3, 9);
+    bballPlaytime = getRand(3, 9);
+    footballPlaytime = getRand(5, 8);
 
     struct player* bplayers = malloc(36 * sizeof(struct player));
     struct player* fplayers = malloc(44 * sizeof(struct player));
